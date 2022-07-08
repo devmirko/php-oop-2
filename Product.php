1547f791307b000e97ee3961153e2c4a9f5c3ef3
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-07-08 16:32:21 +0200

    inizio pregetto

---
+++ Product.php
@@ -0,0 +1,16 @@
+<?php
+
+class Product {
+    public $name;
+    public $price;
+    public $codebar;
+    public $quantity;
+    public $animal_gen;
+
+    public function __construct(string $name, int $price, int $codebar, int $quantity, string $animal_gen)
+    {
+        
+        
+    }
+
+}
